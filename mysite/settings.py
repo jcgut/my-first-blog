de9f51359f665375f1b953e0e51bde430b1b27f8
jcgut <jcga_1994@hotmail.com>
2019-02-12 20:53:45 +0000

    Permitir Acceso Desde Internet

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'io2mwtul0lqcydk%vz-3bl%t!3q@gibvp3ve3gn&x$^=c#h!*^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com', '10.42.22.43', "10.42.22.114"]
+ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com', '10.42.22.43', "10.42.22.114", 'jcgut.pythonanywhere.com']
 
 
 # Application definition
